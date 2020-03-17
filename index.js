e6b0fe9fc148e5c6f0b2b35dda8a23277d0e5040
Prajaktaamare <amare.prajkta@gmail.com>
2020-03-17 16:37:59 +0530

    mongo operations

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-let mongoose = require("mongoose");
+//TO CREATE INSTANCE OF MONGOOSE
+let mongoose = require("mongoose"); 
 
+//TO CONNECT TO MONGODB
 mongoose
     .connect("mongodb://localhost/pra",{ useNewUrlParser: true,useUnifiedTopology: true  })
     .then(() => console.log("db got connected"))
     .catch(error => console.log("something went wrong", error.message));
 
+//CREATE SCHEMA
     let customersSchema = new mongoose.Schema({
     _id: { type: String },
     tags: [String],
@@ -16,33 +19,35 @@ mongoose
     }
 });
 
+//CREATE MODEL
 let customersModel = mongoose.model("customers", customersSchema);
 
+//FUNCTION TO FETCH DATA
 async function FetchCustomers1() {
     let customers1 = await customersModel
-    .find({isPublished: true})
+    .find({isPublished: true}) //TO FIND RECORD FROM DATA
     .find({
         tags: 
         {
-            $nin: ["frontend"]
+            $nin: ["frontend"] //DOESNT INCLUDE THE SPECIFIED DATA
         }
     })
-    .sort("name")
-    .find({}).select("-tags -_id -price -date -isPublished");
+    .sort("name") //SORT BY ALPHABETICAL ORDER 
+    .find({}).select("-tags -_id -price -date -isPublished"); //HERE "-" IS USED TO EXCLUDE PARTICULAR RECORD 
     console.log(customers1);
 }
-FetchCustomers1();
+FetchCustomers1(); //CALLING FUNCTION 
 
 async function FetchCustomers2(){
     let customers2 = await customer2Model
-    .find({isPublished: true})
+    .find({isPublished: true}) 
     .find({
         tags: 
         {
-            $in: ["frontend","backend"]
+            $in: ["frontend","backend"]  //INCLUDE THE SPECIFIED DATA
         }
     })
-    .sort("-price")
+    .sort("-price") //SORT IN DESCENDING ORDER
     .find({}).select("-tags -_id -price -date -isPublished");
     console.log(customers2); 
 }
